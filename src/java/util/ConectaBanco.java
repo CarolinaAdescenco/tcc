5598ed756e71a29b262124a9c871eacbceceeaa0
Rafael Acioly <rafael.acioly@luizalabs.com>
2019-10-04 11:44:34 -0300

    atualiza conexao com banco

--- src/java/util/ConectaBanco.java
+++ src/java/util/ConectaBanco.java
@@ -8,7 +8,7 @@ public class ConectaBanco {
     
     public static Connection getConnection(){
         try{
-            Class.forName("org.postgresql.Driver");            
+            Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection("jdbc:postgresql://localhost:15432/postgres", "postgres", "Postgres2019!");
         } catch(ClassNotFoundException | SQLException erro1){
             throw new RuntimeException(erro1);
